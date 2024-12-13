0036c3841fa251d2cd3ca0844a4ecf922ac00a04
Jackie <dreamerdreamer0127@gmail.com>
2024-12-13 18:24:23 +0700

    11

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.conf.global_settings import STATIC_ROOT, STATICFILES_DIRS
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -130,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
